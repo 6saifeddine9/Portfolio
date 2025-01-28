7ed330c1b342f222c2fbf0fde81b03f72b737bd8
6saifeddine9 <6saifeddine9@gmail.com>
2025-01-28 07:06:10 +0100

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', event => {
+
     // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
@@ -15,39 +16,10 @@ window.addEventListener('DOMContentLoaded', event => {
     );
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+            if (window.getComputedStyle(navbarToggler).display !== 'none' && !responsiveNavItem.id.includes('google_translate_element')) {
                 navbarToggler.click();
             }
         });
     });
 
-    // Fonction pour vérifier si la barre de navigation est ouverte
-    function isNavbarOpen() {
-        return navbar.classList.contains('show');
-    }
-
-    // Fonction pour ouvrir la barre de navigation
-    function openNavbar() {
-        if (!isNavbarOpen()) {
-            navbar.classList.add('show');
-            console.log('Navbar opened');
-        }
-    }
-
-    // Écouteur d'événement pour détecter les changements de traduction
-    const translateElement = document.getElementById('google_translate_element');
-    if (translateElement) {
-        translateElement.addEventListener('click', function() {
-            console.log('Translation element clicked');
-            setTimeout(openNavbar, 500); // Attendre un peu pour que la traduction se termine
-        });
-    } else {
-        console.error('Google Translate element not found');
-    }
-
-    // Écouteur d'événement pour détecter la fin de la traduction
-    window.addEventListener('google-translate-finished', function() {
-        console.log('Translation finished');
-        openNavbar();
-    });
 });
